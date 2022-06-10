0e36bb565d01761b40e913a13b77af6a2781f8c5
Bruno D'Ambrosio <brunodambrosio@pop-os.localdomain>
2022-06-10 12:12:48 -0300

    a

--- Proyecto ODC 2 Animación/app.s
+++ Proyecto ODC 2 Animación/app.s
@@ -1,11 +1,7 @@
 .equ SCREEN_WIDTH, 		640
 .equ SCREEN_HEIGH, 		480
 .equ BITS_PER_PIXEL,  	32
-.equ ancho,				100
-.equ alto,				80
-.equ pared_derecha, 	540 // SCREEN_WIDTH - ancho
-.equ piso, 				400 // SCREEN_HEIGH - alto
-
+				
 .include "dvdfuns.s"
 
 .globl main
@@ -18,95 +14,18 @@ main:
 	mov x15, 3	//vector movimiento en y
 	mov x14, 3	//vector movimiento en x
 	
-	mov x5, 500		//pos.columna inicial
+	mov x5, 500	//pos.columna inicial
 	mov x6, 300	//pos.fila inicial
-	movz x7, 0x10, lsl 16
-	movk x7, 0x0000, lsl 00
+	
 	movz x11, 0xff,lsl 16  
 	movk x11, 0xffff, lsl 00
 	
 
 
-DVDLOOP:
-	// FONDO
-	movz x10, 0x00,lsl 16  // Color de fondo : Negro
-	movk x10, 0x00ff, lsl 00
+LOOP:
 	
-	mov x1, SCREEN_WIDTH
-	mov x2, SCREEN_HEIGH
-	mov x3, 0	// pos.columna
-	mov x4, 0	// pos.fila
-
-	BL Pixeldir
-	BL dibujarcuadrado
-
-//BLoque
-	movz x10, 0x57,lsl 16  // Color de fondo : Celeste
-	movk x10, 0xb7ff, lsl 00
-
-	mov x1, ancho
-	mov x2, alto
-
-	mov x3, x5	// pos.columna (x)
-	cmp x3, pared_derecha //  pos.x == pared derecha
-	B.GE rebote_horizontal
-	cmp x3, 0
-	B.LE rebote_horizontal
-mov_x:
-	add x3, x3, x14
-	
-	mov x4, x6	// pos.fila (y)
-	cmp x4, piso  // pos.y == piso
-	B.GE rebote_vertical	
-	cmp x4, 0
-	B.LE rebote_vertical	
-mov_y:
-	add x4, x4, x15
-	mov x10, x11
-	BL Pixeldir
-	BL dibujarlogo
-
-	mov x5, x3
-	mov x6, x4
-
+	BL dibujarfondo
+	BL cuadrado
 	BL DELAY
-	
-	b DVDLOOP
+	B LOOP
 
-rebote_horizontal:
-	mov x13, 0
-	sub x14, x13, x14
-	/* Cambia de color */
-	mul x9, x14, x15 
-	cmp x9, x13
-	B.LE resto_color
-	B sumar_color
-resto_color:
-	movz x13, 0xaa, lsl 16
-	movk x13, 0x0255, lsl 0 
-	sub x11, x11, x13
-	b mov_x
-sumar_color:
-	movz x13, 0xae, lsl 16
-	movk x13, 0xa2a5, lsl 0 
-	add x11, x11, x13	
-	b mov_x
-
-rebote_vertical:
-	mov x13, 0
-	sub x15, x13, x15
-	/* Cambia de color */
-	mul x9, x14, x15 
-	cmp x9, x13
-	B.GE resto_colorv
-	B sumar_colorv
-resto_colorv:
-	movz x13, 0x33, lsl 16
-	movk x13, 0x2e95, lsl 0 
-	sub x11, x11, x13
-	b mov_y
-sumar_colorv:
-	movz x13, 0x45, lsl 16
-	movk x13, 0x7d85, lsl 0 
-	add x11, x11, x13
-	b mov_y
--- Proyecto ODC 2 Animación/dvdfuns.s
+++ Proyecto ODC 2 Animación/dvdfuns.s
@@ -1,6 +1,10 @@
 .equ SCREEN_WIDTH, 		640
 .equ SCREEN_HEIGH, 		480
 .equ BITS_PER_PIXEL,  	32
+.equ ancho,				100
+.equ alto,				80
+.equ pared_derecha, 	540 // SCREEN_WIDTH - ancho
+.equ piso, 				400 // SCREEN_HEIGH - alto
 
 
  Pixeldir:
@@ -11,6 +15,14 @@
 	add x0, x0, x20
  ret	
  
+ 
+ Pixeldir2:
+	mov x0, 640			
+	mul x0, x0, x8						
+	add x0, x0, x7						
+	lsl x0, x0, 2						 
+	add x0, x0, x20
+ ret
  
  
  dibujartriangulo: 
@@ -27,7 +39,7 @@
      	 mov x11, x12
      	 
      	coloreart:	
-	  stur w10,[x12]	   
+	  stur w25,[x12]	   
 	  add x12,x12,4	   
 	  sub x9,x9,1	  
 	  cbnz x9,coloreart
@@ -70,6 +82,10 @@
 	ldur lr, [sp] // Recupero el puntero de retorno del stack
         add sp, sp, #8
         br lr
+        
+ 
+
+	
 	 
  DELAY:
  	sub sp, sp, #8 // Guardo el puntero de retorno en el stack
@@ -85,146 +101,113 @@
 
 
  	
-dibujarsemicirculo:
-	/*
-	parametros:
-				centro = (x3, x4);  x3 es la columna, x4 la fila
-				radio = x1
-				w10 = color
-						
-	comportamiento:
-				x0 empieza en esquina superior izquierdo del cuadrado que contiene el circulo
-				y va recorriendo el cuadrado y si se cumple que x0 esta dentro del circulo dado por
-					(x0.fila - x3)*2 + (x0.columna - x4)2 <= x1*2
-					\------x9-----/     \------x11-----/   \--x12--/
-					   \-------------x13-------------/							 
-				se pinta el pixel
 
-*/              
-
-                
-		
-		
-		sub sp, sp, #8 // Guardo el puntero de retorno en el stack
-      		stur lr, [sp]
-      		
-      		
-      		mov x7, x3
-		mov x8, x4
-
-		add x5, x4,x1		// con x5 me fijo si llego a final de Y
-		add x6, x3,x1		// con x6 me fijo si llega al final de X
-					          
-		
-		mov x15, x3
-		mov x16, x4
-			
-		
-resetsY:
-		cmp x3, x6
-		b.gt ends
-		sub x4, x5, x1
-		sub x4, x4, x1      // Inicio Y
-cirloops:
-		cmp x4,x5
-		b.eq next_filas
-
-		sub x9, x3, x15			   
-		mul x9, x9, x9				// x9 = (x0.fila - x3)**2 	
-		
-		sub x11, x4, x16			   
-		mul x11, x11, x11			// x11 = (x0.columna - x4)**2
-
-		add x13, x9, x11			// x13 = (x0.fila - x1)*2 + (x0.columna - x2)*2
-
-		mov x12, x1
-		mul x12, x12, x12			// x12 = radio**2
-
-		cmp x12, x13
-		B.GE colors					//si x13 <= x12 
-		B skips
-colors:		
-		BL Pixeldir				// then pintar pixel
-		stur w10,[x0]	
-skips:
-		add x4, x4, #1				// avanza pixel
-		B cirloops
-next_filas:
-		add x3, x3, #1
-		B resetsY
-ends:
-	mov x3, x7
-	mov x4, x8
+dibujarfondo:    w10 = color
+ 
+ 	sub sp, sp, #8 // Guardo el puntero de retorno en el stack
+      	stur lr, [sp]
+      	movz x10, 0x00,lsl 16  // Color de fondo 
+	movk x10, 0x00ff, lsl 00
+	mov x3, 0	// pos.columna
+	mov x4, 0	// pos.fila
+	BL Pixeldir
+ 	mov x1, 640
+ 	mov x2, 60
+ 	mov x18, 480
+ 	loopd:
+ 	BL dibujarcuadrado
+	mov x2, 60
+        add x4, x4, 60
+        BL Pixeldir
+        sub x10, x10, 30
+        cmp x4, x18
+        B.NE loopd
+        
+       
+        
         ldur lr, [sp] // Recupero el puntero de retorno del stack
-        add sp, sp, #8 
-
+        add sp, sp, #8
         br lr
-		
+       
 
-	
-
-	
-dibujarlogo:	
-	
+cuadrado:
 	sub sp, sp, #8 // Guardo el puntero de retorno en el stack
       	stur lr, [sp]
+        mov x1, ancho
+	mov x2, alto
+    	mov x3, x5	// pos.columna (x)
+	cmp x3, pared_derecha //  pos.x == pared derecha
+	B.GE rebote_horizontal1
+	cmp x3, 0
+	B.LE rebote_horizontal1
+ 
+        mov_x:
+        
+	add x3, x3, x14
+	mov x4, x6	// pos.fila (y)
+	cmp x4, piso  // pos.y == piso
+	B.GE rebote_vertical1
+	cmp x4, 0
+	B.LE rebote_vertical1	
+	
+	mov_y:
+	add x4, x4, x15
+	mov x10, x11
+	BL Pixeldir
 	BL dibujarcuadrado
-	mov x17, x3
-	mov x18, x4
-	movz x10, 0x00, lsl 16
-	movk x10, 0x0000, lsl 00
-	add x4, x4, 40
-	add x3 , x3, 10
-	mov x1, 20
-	BL Pixeldir
-	BL dibujarsemicirculo
-	add x3, x3, 5
-	mov x1, 10
-	movz x10, 0xff, lsl 16
-	movk x10, 0xffff, lsl 00
-	BL dibujarsemicirculo
-	movz x10, 0x00, lsl 16
-	movk x10, 0x0000, lsl 00
-	mov x2,18
-	mov x3, x17
-	mov x4, x18
-	add x4, x4, 50
-	add x3 , x3, 50 
-	BL Pixeldir
-	BL dibujartriangulo
-	sub x4, x4, 7
-	mov x2, 12
-	movz x10, 0xff, lsl 16
-	movk x10, 0xffff, lsl 00
-	BL Pixeldir
-	BL dibujartriangulo
+	mov x5, x3
+	mov x6, x4
+	B ending
 	
 	
+	rebote_horizontal1:
+		mov x13, 0
+		sub x14, x13, x14
+		/* Cambia de color */
+		mul x9, x14, x15 
+		cmp x9, x13
+		B.LE resto_color
+		B sumar_color
 	
 	
+		resto_color:
+			movz x13, 0xaa, lsl 16
+			movk x13, 0x0255, lsl 0 
+			sub x11, x11, x13
+			b mov_x
 	
+	
+		sumar_color:
+			movz x13, 0xae, lsl 16
+			movk x13, 0xa2a5, lsl 0 
+			add x11, x11, x13	
+			b mov_x
 
-	mov x3, x17
-	mov x4, x18
-	add x4, x4, 50
-	add x3 , x3, 75     // 10-d(20)-10-v(20)-10-d-10-
-	mov x1, 20
-	movz x10, 0x00, lsl 16
-	movk x10, 0x0000, lsl 00
-	BL Pixeldir
-	BL dibujarsemicirculo
-	add x3, x3, 5
-	mov x1, 10
-	movz x10, 0xff, lsl 16
-	movk x10, 0xffff, lsl 00
-	BL dibujarsemicirculo
+	rebote_vertical1:
+		mov x13, 0
+		sub x15, x13, x15
+		/* Cambia de color */
+		mul x9, x14, x15 
+		cmp x9, x13
+		B.GE resto_colorv
+		B sumar_colorv
+		
+		resto_colorv:
+			movz x13, 0x33, lsl 16
+			movk x13, 0x2e95, lsl 0 
+			sub x11, x11, x13
+			b mov_y
 	
+		sumar_colorv:
+			movz x13, 0x45, lsl 16
+			movk x13, 0x7d85, lsl 0 
+			add x11, x11, x13
+			b mov_y
 	
+	 ending:	
+		 ldur lr, [sp] // Recupero el puntero de retorno del stack
+      		  add sp, sp, #8
+      		  br lr
 	
-	ldur lr, [sp] // Recupero el puntero de retorno del stack
-        add sp, sp, #8 
-
-        br lr
- 
- 
+			
 
